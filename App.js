e7a5a6a4c247bd9368a264121bd24ffea1688d74
Zolon Wilkins <zolonsk8@gmail.com>
2018-12-31 21:52:36 -0600

    fancy modal

--- App.js
+++ App.js
@@ -7,13 +7,14 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button, Alert, SafeAreaView, TouchableOpacity, Dimensions, ActivityIndicator, AsyncStorage } from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, Alert, SafeAreaView, TouchableOpacity, TouchableHighlight, Dimensions, ActivityIndicator, AsyncStorage, Modal } from 'react-native';
 import Mapbox from '@mapbox/react-native-mapbox-gl';
 Mapbox.setAccessToken('pk.eyJ1Ijoiem9sb24iLCJhIjoiY2pxY3ZucGFlM20zbTQ4bjIwaWl1eGw5NCJ9.z9-BvSlFUuNxVVqwuz11Sw');
 
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
-
+console.log(width)
+console.log(height)
 type Props = {};
 export default class App extends Component<Props> {
 
@@ -22,7 +23,7 @@ export default class App extends Component<Props> {
     this.state = {
       latitude: 22.3827448,
       longitude: 114.139249,
-      isLoading: true
+      modalVisible: false
     }
     this._storeData('token', 'zolon')
     this.centerMap = this.centerMap.bind(this)
@@ -34,13 +35,16 @@ export default class App extends Component<Props> {
     this.setUserLocation()
   }
 
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
   setUserLocation() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({
           latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          isLoading: false
+          longitude: position.coords.longitude
         });
       },
       (error) => console.log(error),
@@ -95,9 +99,35 @@ export default class App extends Component<Props> {
           onUserLocationUpdate={this.onUserLocationUpdate.bind(this)}
           style={styles.map}>
           <SafeAreaView>
+            <Modal
+              animationType="slide"
+              visible={this.state.modalVisible}
+              style={styles.modal}
+              transparent={true}
+              shouldCloseOnOverlayClick={true}
+              onRequestClose={() => { this.setModalVisible(!this.state.modalVisible) }}>
+              <View style={styles.modalContent}>
+                <View style={styles.modalHeader}>
+                  <Text style={{fontFamily: 'NunitoSans-Black', fontSize: 20, textAlign: "center"}}>Log In</Text>
+                  <TouchableOpacity style={styles.button} onPress={() => { this.setModalVisible(!this.state.modalVisible); }}>
+                    <Text style={styles.buttonTextSmall}>Close</Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.modalBody}>
+                </View>
+              </View>
+            </Modal>
             <TouchableOpacity style={styles.button} onPress={this.centerMap}>
               <Text style={styles.buttonText}>Center Map</Text>
             </TouchableOpacity>
+            { this.state.token && (
+              <TouchableOpacity
+                style={styles.logInButton}
+                onPress={() => { this.setModalVisible(true);}}
+                >
+                <Text style={styles.buttonText}>Log In</Text>
+              </TouchableOpacity>
+            )}
           </SafeAreaView>
         </Mapbox.MapView>
       </View>
@@ -127,9 +157,46 @@ const styles = StyleSheet.create({
     marginHorizontal: 'auto',
     borderRadius: 15
   },
+  logInButton: {
+    alignSelf:'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+    paddingVertical: 10,
+    marginTop: 3,
+    paddingHorizontal: 15,
+    width: 'auto',
+    marginHorizontal: 'auto',
+    borderRadius: 15
+  },
   buttonText: {
     color: 'white',
     fontSize: 20,
     fontFamily: 'NunitoSans-Black',
+  },
+  buttonTextSmall: {
+    color: 'white',
+    fontSize: 10,
+    fontFamily: 'NunitoSans-Black',
+  },
+  modal: {
+    flex: 1
+  },
+  modalContent: {
+    fontFamily: 'NunitoSans-Black',
+    padding: 20,
+    alignItems: 'center',
+    marginTop: 100,
+    backgroundColor: 'white',
+    borderRadius: 15,
+    height: height / 2
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+  modalBody: {
+    
   }
 });
